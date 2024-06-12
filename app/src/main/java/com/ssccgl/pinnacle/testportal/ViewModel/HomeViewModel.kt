0511fa2f5a9341b648b423673e28bada9c506a51
riteshpinnacle <162966937+riteshpinnacle@users.noreply.github.com>
2024-06-12 12:44:55 +0530

    Update HomeViewModel.kt
    Changes by Ritesh_2

--- app/src/main/java/com/ssccgl/pinnacle/testportal/ViewModel/HomeViewModel.kt
+++ app/src/main/java/com/ssccgl/pinnacle/testportal/ViewModel/HomeViewModel.kt
@@ -19,21 +19,22 @@ class HomeViewModel : ViewModel() {
         viewModelScope.launch {
             _uiState.value = HomeUiState(
                 items = listOf(
-                    HomeItem(R.drawable.ic_testportal, "Test Portal"),
+                    HomeItem(R.drawable.ic_test_portal, "Test Portal"),
                     HomeItem(R.drawable.ic_product, "Product"),
                     HomeItem(R.drawable.ic_blog, "Blog"),
-                    HomeItem(R.drawable.ic_course, "My Courses"),
-                    HomeItem(R.drawable.ic_testportal, "Attempted Tests"),
-                    HomeItem(R.drawable.ic_testportal, "Tracking"),
-                    HomeItem(R.drawable.ic_testportal, "eBooks"),
-                    HomeItem(R.drawable.ic_testportal, "Books"),
-                    HomeItem(R.drawable.ic_testportal, "Saved"),
-                    HomeItem(R.drawable.ic_testportal, "Offline EBooks"),
-                    HomeItem(R.drawable.ic_testportal, "QR Scanner"),
-                    HomeItem(R.drawable.ic_testportal, "Typing Software"),
+                    HomeItem(R.drawable.ic_my_courses, "My Courses"),
+                    HomeItem(R.drawable.ic_attempted_tests, "Attempted Tests"),
+                    HomeItem(R.drawable.ic_tracking, "Tracking"),
+                    HomeItem(R.drawable.ic_ebooks, "eBooks"),
+                    HomeItem(R.drawable.ic_books, "Books"),
+                    HomeItem(R.drawable.ic_bookmarked, "Bookmarked"),
+                    HomeItem(R.drawable.ic_offline_ebook, "Offline EBooks"),
+                    HomeItem(R.drawable.ic_qr_scanner, "QR Scanner"),
+                    HomeItem(R.drawable.ic_typing_software, "Typing Software"),
 //                    HomeItem(R.drawable.ic_offline_videos, "Offline Videos")
                 )
             )
         }
     }
+
 }
